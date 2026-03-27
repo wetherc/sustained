f1ce67b5acfa3e349bbbd28212571429dbde5cb7
Christopher Wetherill <git@tbmh.org>
2026-03-27 18:20:28 -0400

    feat(typing): Fix typing stub inclusion
    Adds a `py.typed` marker file and configures `pyproject.toml`
    for `hatchling` to correctly include all type information in both
    editable installs and wheel distributions

--- hatch_build.py
+++
@@ -1,7 +0,0 @@
-from hatchling.builders.hooks.plugin.interface import BuildHookInterface
-
-
-class CustomBuildHook(BuildHookInterface):
-    def initialize(self, version, build_data):
-        # Add .pyi and py.typed files to the shared source
-        build_data["force_include"]["src/sustained"] = "sustained"
